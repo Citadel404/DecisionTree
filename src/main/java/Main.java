b213e1c18efaf6fbdd47bf52a75b5d4b782fccf5
Ruilin Yang <973969022@qq.com>
2024-07-22 14:35:55 +0800

    Init Commited

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Author: 973969022@qq.com
+ * Date: 2024/7/22 14:15
+ * 主类
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/cn/rlsblog/DecisionTree.java
@@ -0,0 +1,10 @@
+package cn.rlsblog;
+
+/**
+ * Author: 973969022@qq.com
+ * Date: 2024/7/22 14:15
+ * 决策树类
+ */
+public class DecisionTree {
+
+}
